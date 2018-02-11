42b7f8646d511ef03be71502561438005f022962
Unknown <smirnov@informatics.ru>
2018-02-11 12:29:38 +0300

    DB credentials

--- morris_butler/settings.py
+++ morris_butler/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'morris_butler.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'morris_db',
+        'USER': 'morris_user',
+        'PASSWORD': 'morris_pass',
     }
 }
 
