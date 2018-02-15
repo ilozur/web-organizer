d429310c8d8e74589c5704a9d5ce30a15b1a2a73
Bryantsev Stepan <steperb@gmail.com>
2018-02-16 00:21:18 +0300

    Added table notes

--- main/models.py
+++ main/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-import sqlite3
-from django.contrib.auth.models import User
 
 # Create your models here.
-class Notes(models.Model):
-    data = models.CharField(max_length=200)
-    user = models.ForeignKey(User, default=1, on_delete=set([1, ]))
-    name = models.CharField(max_length=128, default="title")
-    added_time = models.CharField(max_length=25, default=0)
-
