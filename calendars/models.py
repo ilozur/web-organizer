bc4e8cae466197efd4ae579bd0f6e4341294b666
Kutilin Sergei <igameskut@icloud.com>
2018-02-16 23:32:25 +0300

    Added event in BD

--- calendars/models.py
+++ calendars/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Event(models.Model):
+    user = models.ForeignKey(User, default=1, on_delete=set([1, ]))
+    date = models.DateField()
+    time = models.TimeField()
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_public = models.BooleanField()
