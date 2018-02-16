f203bff9a5497b547f35ad84fe6953684e2f005f
Bryantsev Stepan <steperb@gmail.com>
2018-02-16 16:29:49 +0300

    Created addding note page

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from notes.views import index
+from notes.views import index, add_note
 
 urlpatterns = [
     path('', index, name='notes.index'),
+    path('add', add_note, name='notes.add_note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
-
 def index(request):
     context = {
         'title': "Notes index page",
         'header': "Notes index page header",
     }
     return render(request, "notes/index.html", context)
+
+def add_note(request):
+    context = {
+        'header': "Add_note page"
+    }
+    return render(request, "notes/add_note.html", context)
