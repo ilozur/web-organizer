483b3dbc01aeefe86f108ae68eb57ed049ac889b
Bryantsev Stepan <steperb@gmail.com>
2018-02-16 19:36:03 +0300

    Added setting auto_now_add time in models.py, some changes in db structure

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-import sqlite3
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -7,5 +6,4 @@ class Notes(models.Model):
     data = models.CharField(max_length=200)
     user = models.ForeignKey(User, default=1, on_delete=set([1, ]))
     name = models.CharField(max_length=128, default="title")
-    added_time = models.CharField(max_length=25, default=0)
-
+    added_time = models.DateField(auto_now_add=True)
