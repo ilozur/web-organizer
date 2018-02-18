5070830f6038ca833c5971993b3ec8e7cd586d43
Nikita Kuznecov <Music_Life_Good@mail.ru>
2018-02-18 12:48:47 +0300

    added Todo base

--- todolist/models.py
+++ todolist/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 
 class Todos(models.Model):
     user = models.ForeignKey(User, default=1, on_delete=set([1, ]))
-    added_time = models.CharField(max_length=100, default=0)
-    added_date = models.CharField(max_length=100, default=0)
-    priority = models.CharField(max_length=1, default="priority")
+    added_time = models.TimeField(max_length=20, default='2018-01-01')
+    added_date = models.DateField(max_length=20, default='00-00')
+    priority = models.CharField(max_length=100, default="priority")
     status = models.CharField(max_length=128, default="in progress")
     deadline = models.CharField(max_length=128, default="deadline")
-    text = models.CharField(max_length=200)
-    title = models.CharField(max_length=128)
+    text = models.CharField(max_length=200, default="time")
+    title = models.CharField(max_length=128, default="title")
