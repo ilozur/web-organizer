ed7421ba168b3be4ef02ecfd919a1fa765d418da
Andrey <Ryabov.a80@mail.ru>
2018-02-25 12:45:36 +0300

    issues #18 added func show_todo

--- todolist/views.py
+++ todolist/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 import os
+
+from todolist import models
 from todolist.forms import AddTodoForm
 from todolist.models import Todos
 from django import forms
@@ -17,27 +19,36 @@ def index(request):
     }
     return render(request, "todolist/index.html", context)
 
+
 @login_required
 def add_todo(request):
-        context = {}
-        context['user'] = request.user
-        if request.POST:
-            form = AddTodoForm(request.POST)
-            if form.is_valid():
-                user = request.user
-                time_for_todo()
-                date_for_todo()
-                p = Todos(text=form.data['text'], user=user, title=form.data['title'], added_time=time_for_todo(),
-                          added_date=date_for_todo(), priority=form.data['priority'], deadline=form.data['deadline'])
-                p.save()
-                context['id'] = p.id
-            else:
-                context['errors'] = form.errors
-            return render(request, 'add_result.html', context)
+    context = {}
+    context['user'] = request.user
+    if request.POST:
+        form = AddTodoForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            time_for_todo()
+            date_for_todo()
+            p = Todos(text=form.data['text'], user=user, title=form.data['title'], added_time=time_for_todo(),
+                      added_date=date_for_todo(), priority=form.data['priority'], deadline=form.data['deadline'])
+            p.save()
+            context['id'] = p.id
         else:
-            form = AddTodoForm()
-            context['add_form'] = form
-            return render(request, 'add.html', context)
+            context['errors'] = form.errors
+        return render(request, 'add_result.html', context)
+    else:
+        form = AddTodoForm()
+        context['add_form'] = form
+        return render(request, 'add.html', context)
+
+
+def show_todo(request, id):
+    context = {}
+    context['user'] = request.user
+    todo_now = Todos.objects.filter(id=id).first()
+    context['a'] = todo_now 
+    return render(request, 'show.html', context)
 
 
 def time_for_todo():
@@ -46,10 +57,10 @@ def time_for_todo():
     todo_time = []
     spaces = 0
     for i in range(len(timer)):
-            if timer[i] == ' ':
-                spaces += 1
-            if spaces == 2:
-                todo_time.append(timer[i])
+        if timer[i] == ' ':
+            spaces += 1
+        if spaces == 2:
+            todo_time.append(timer[i])
     return todo_time
 
 
@@ -59,8 +70,8 @@ def date_for_todo():
     todo_date = []
     spaces = 0
     for i in range(len(date)):
-            if date[i] == ' ':
-                spaces += 1
-            if (spaces>2) or (spaces<2):
-                todo_date.append(date[i])
+        if date[i] == ' ':
+            spaces += 1
+        if (spaces > 2) or (spaces < 2):
+            todo_date.append(date[i])
     return todo_date
