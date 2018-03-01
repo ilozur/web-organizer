d359f305687f8b1baa10bfacdad759ff83d84c8f
Юрин Дмитрий <super.super3337@yandex.ru>
2018-03-01 21:13:58 +0300

    Update views.py

--- todolist/views.py
+++ todolist/views.py
@@ -7,3 +7,27 @@ def index(request):
         'header': "Todolist index page header"
     }
     return render(request, "todolist/index.html", context)
+ 
+ def show_todolist(request, id):
+    context = {}
+    if request.POST:
+        form = ShowTodolistForm(request)
+        Todo = Todolist.objects.filter(id=id).first()
+        Todolist.data = request.POST['data']
+        Todolist.save()
+        return HttpResponseRedirect('/todolist')
+    else:
+        if len(todolist.objects.filter(id=id)) > 0:
+            todo = todolist.objects.filter(id=id).first()
+            context = {
+                'header': "Show note page header",
+                'id': id,
+                'title': note.name
+                'priority' :
+                'status':
+            }
+            form = ShowTodolistForm({'data': todolist.data})
+            context['form'] = form
+        else:
+            context['error'] = True
+        return render(request, "notes/show_todolist.html", context)
