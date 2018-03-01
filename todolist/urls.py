262a19a913b6addfd47ad7fe365c755edc64ea6e
Юрин Дмитрий <super.super3337@yandex.ru>
2018-03-01 21:14:12 +0300

    Update urls.py

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,4 +4,6 @@ from todolist.views import index
 
 urlpatterns = [
     path('', index, name='todolist.index'),
+    path('add', add_todolist, name='todolist.add_todolist'),
+    path('show/<int:id>', show_todolist, name='todo.show_todolist'),
 ]
