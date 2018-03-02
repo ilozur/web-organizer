ab31e673be912d5a62170b4d788ab0a77dbf2197
Nikita Kuznecov <Music_Life_Good@mail.ru>
2018-03-02 20:23:40 +0300

    updated urls

--- todolist/urls.py
+++ todolist/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('', index, name='todolist.index'),
     path('add', views.add_todo, name='todolist.add_todo'),
     path('done', views.completed_todos, name='todolist.completed_todos'),
-    path('add', views.add_todo, name='add_todo'),
-    path('show/<int:id>', views.show_todo, name='show_todo')
+    path('show/<int:id>', views.show_todo, name='todolist.show_todo')
 ]
