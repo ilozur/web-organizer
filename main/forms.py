88200511c30d3a37ea543807adab49cc951f2d22
Bryantsev Stepan <steperb@gmail.com>
2018-03-07 00:25:47 +0300

    Added design of index page when user is authenticated and not authenticated; header was connected to site; login and registration fanctions were connected to new forms; now login and registration are on one page; all static files were placed into folder static/.

--- main/forms.py
+++ main/forms.py
@@ -8,9 +8,9 @@ class SignInForm(forms.Form):
 
 
 class SignUpForm(forms.Form):
-    email = forms.EmailField(label='Enter your e-mail', max_length=200)
-    username = forms.CharField(label='Enter username', max_length=200)
-    name = forms.CharField(label='Enter your name', max_length=200)
-    surname = forms.CharField(label='Enter your surname', max_length=200)
-    password1 = forms.CharField(label='Enter password', max_length=200, widget=forms.PasswordInput())
-    password2 = forms.CharField(label='Enter password again', max_length=200, widget=forms.PasswordInput())
+    email = forms.EmailField(label='E-mail', max_length=200, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your e-mail'}))
+    username = forms.CharField(label='Username', max_length=200, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your username'}))
+    name = forms.CharField(label='Name', max_length=200, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your name'}))
+    surname = forms.CharField(label='Surname', max_length=200, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your surname'}))
+    password1 = forms.CharField(label='Password', max_length=200, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password'}))
+    password2 = forms.CharField(label='Password again', max_length=200, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password again'}))
