d4a25c3990ad28466736003c4f49069c34d20dc9
Nikita Kuznecov <Music_Life_Good@mail.ru>
2018-03-18 12:48:37 +0300

    deleted statics

--- static_root/js/calendar.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('.btn_change').click(function(event) {
-		$('.table').removeClass('active')
-		var num = $(this).attr('data-num');
-		$('#table'+num).addClass('active')
-	});
-});
