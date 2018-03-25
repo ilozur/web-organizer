ffe0110ff3eb3531eef02e4d0cd531644ec6d4e0
roman <romanecz00@gmail.com>
2018-03-25 12:12:25 +0300

    added tests from lecture

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+
+class TestMainPage ( TestCase ):
+
+    def setUp( self ):
+        u = User(username='testuser', is_active=1)
+        u.set_password('password123')
+        self.c = Client()
+
+    def test_redirect(self):
+        self.c.get('/')
+        response = self.c.post('/', {
+            'user': 'test',
+            'pass': '123454321'
+        })
+        self.assertRedirects(response, '/')
+
+    def test_non_auth_template(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEqual ( response.status_code, 200 )
+        self.assertTemplateUsed(response, 'main/index.html')
+
+    def test_auth_html ( self ):
+        self.c.login (username='testuser', password = 'password123')
+        response = self.c.get('/')
+        self.assertContains ( response, 'Добрый день' )
+        self.assertContains ( response, 'Последние заметки' )
+        self.assertContains ( response, 'Ближайшие события календаря' )
