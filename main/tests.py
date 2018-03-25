f346234e0244b6c8b41af57d6ebdee052ad39256
roman <romanecz00@gmail.com>
2018-03-25 13:21:54 +0300

    added json file w/ users

--- main/tests.py
+++ main/tests.py
@@ -22,12 +22,12 @@ class TestMainPage ( TestCase ):
     def test_non_auth_template(self):
         c = Client()
         response = c.get('/')
-        self.assertEqual ( response.status_code, 200 )
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'main/index.html')
 
-    def test_auth_html ( self ):
-        self.c.login (username='testuser', password = 'password123')
+    def test_auth_html(self):
+        self.c.login(username='testuser', password='password123')
         response = self.c.get('/')
-        self.assertContains ( response, 'Добрый день' )
-        self.assertContains ( response, 'Последние заметки' )
-        self.assertContains ( response, 'Ближайшие события календаря' )
+        self.assertContains(response, 'Добрый день')
+        self.assertContains(response, 'Последние заметки')
+        self.assertContains(response, 'Ближайшие события календаря')
