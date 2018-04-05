be78714dbcfa8242d772f50195fe34296d851bb0
roman <romanecz00@gmail.com>
2018-04-05 20:33:12 +0300

    wip, script is raw but seems to be working

--- main/management/commands/create.py
+++ main/management/commands/create.py
@@ -4,7 +4,7 @@ import datetime
 from notes.models import Notes
 
 
-class Command(BaseCommand):
+class Command ( BaseCommand ):
     args = "there's no args"
 
     Lopsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
@@ -12,18 +12,18 @@ class Command(BaseCommand):
                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \
                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, \
                  sunt in culpa qui officia deserunt mollit anim id est laborum."
-    def creation( self ):
+
+    def creation ( self ):
         users = []
-        for i in range (0, 50):
-            users.append(self.create_user(username = 'test_user_{}'.format(i), password = " ", email = None))
-            for j in range(0, i):
-                self.create_note(text=self.Lopsum, name = "Lorem Ipsum", user = users[i], time=datetime.time)
+        for i in range ( 0, 50 ):
+            users.append ( self.create_user ( username='test_user_{}'.format ( i ), password=" ", email=None ) )
+            for j in range ( 0, i ):
+                self.create_note ( text=self.Lopsum, name="Lorem Ipsum", user=users[i], time=datetime.time )
 
-    def create_user( self, username, password, email ):
-        u = User.objects.create_user(username, email, password)
-        u.save()
+    def create_user ( self, username, password, email ):
+        u = User.objects.create_user ( username, email, password )
+        u.save ()
 
-
-    def create_note( self, text, name, user, time ):
-        n = Notes(data=text, user=user, added_time=time, name=name)
-        n.save()
+    def create_note ( self, text, name, user, time ):
+        n = Notes ( data=text, user=user, added_time=time, name=name )
+        n.save ()
