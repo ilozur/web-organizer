fbfd261e05e78e7c468fe73a5d2973b907ca152d
Мезин Роман <romanecz00@gmail.com>
2018-04-08 13:11:38 +0300

    пре-финальная версия костылей в create.py; нет уничтожения дб перед созданием. БУДЕТ, ДОРОГОЙ ТИМЛИД, БУДЕТ.

--- main/management/commands/create.py
+++ main/management/commands/create.py
@@ -4,23 +4,23 @@ import datetime
 from notes.models import Notes
 from calendars.models import Event
 
+
 class Command ( BaseCommand ):
     args = "there's no args"
-    us = User.object.all()
+    us = User.objects.all ()
     Lopsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
                 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco\
                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \
                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, \
                  sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-    def handle(self, *args, **options):
-        users = []
-        
+    def handle ( self, *args, **options ):
+
         for i in range ( 0, 50 ):
-            self.create_user ( username='test_user_{}'.format ( i+len(us) ), password=" ", email=None )
-            for j in us:
-                self.create_note ( text=self.Lopsum, name="Lorem Ipsum", user=j, time=datetime.now())
-                self.create_calendar(date = "2108-01-01", content=self.Lopsum, user=j)
+            self.create_user ( username='test_user_{}'.format ( i + len ( self.us ) ), password=" ", email=None )
+            for j in self.us:
+                self.create_note ( text=self.Lopsum, name="Lorem Ipsum", user=j, time=datetime.datetime.now() )
+                self.create_calendar ( date="2108-01-01", content=self.Lopsum, user=j )
 
     def create_user ( self, username, password, email ):
         u = User.objects.create_user ( username, email, password )
@@ -30,6 +30,6 @@ class Command ( BaseCommand ):
         n = Notes ( data=text, user=user, added_time=time, name=name )
         n.save ()
 
-    def create_calendar( self, date, content, user):
-        e = Event(user=user, date=date, description=content)
-        e.save()
+    def create_calendar ( self, date, content, user ):
+        e = Event ( user=user, date=date, description=content )
+        e.save ()
