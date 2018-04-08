def7970e3da536081fa2dffcc85e0c20e105fd8d
Мезин Роман <romanecz00@gmail.com>
2018-04-08 09:59:24 +0300

    костыли и велосипеды в create.py

--- main/management/commands/create.py
+++ main/management/commands/create.py
@@ -6,7 +6,7 @@ from calendars.models import Event
 
 class Command ( BaseCommand ):
     args = "there's no args"
-
+    us = User.object.all()
     Lopsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
                 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco\
                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \
@@ -15,20 +15,21 @@ class Command ( BaseCommand ):
 
     def handle(self, *args, **options):
         users = []
+        
         for i in range ( 0, 50 ):
-            self.create_user ( username='test_user_{}'.format ( i ), password=" ", email=None )
-            for j in range ( 0, i ):
-                self.create_note ( text=self.Lopsum, name="Lorem Ipsum", user_id=i, time=datetime.time )
-                self.create_calendar(date = "2108-01-01", content=self.Lopsum, user_id=i)
+            self.create_user ( username='test_user_{}'.format ( i+len(us) ), password=" ", email=None )
+            for j in us:
+                self.create_note ( text=self.Lopsum, name="Lorem Ipsum", user=j, time=datetime.now())
+                self.create_calendar(date = "2108-01-01", content=self.Lopsum, user=j)
 
     def create_user ( self, username, password, email ):
         u = User.objects.create_user ( username, email, password )
         u.save ()
 
-    def create_note ( self, text, name, user_id, time ):
-        n = Notes ( data=text, user=user_id, added_time=time, name=name )
+    def create_note ( self, text, name, user, time ):
+        n = Notes ( data=text, user=user, added_time=time, name=name )
         n.save ()
 
-    def create_calendar( self, date, content, user_id):
-        e = Event(user=user_id, date=date, description=content)
+    def create_calendar( self, date, content, user):
+        e = Event(user=user, date=date, description=content)
         e.save()
