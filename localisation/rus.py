51f0693dfe2e9a215a56345d4cd7ddaad7854eb7
Yacentyuk Nikolai <lyoha_lepyoha@mail.ru>
2018-04-14 23:28:10 +0300

    About 50% of russian localisation

---
+++ localisation/rus.py
@@ -0,0 +1,92 @@
+
+
+-- Главная после авторизации--
+title = 'Главная'
+notes_title = 'Заметки'
+calendars_title = 'Календарь'
+reminders_title = 'Напоминания'
+account_title = 'Личный кабинет'
+quit_title = 'Выйти'
+navigation_title = 'Навигация'
+resources_title = 'Ресурсы'
+social_network_title = 'Соц. сети'
+help_title = 'Справка'
+
+
+nearest_event_title = 'Ближайшее мероприятие'
+closest_reminder_title = 'Скорейшее напоминание'
+latest_note_title = 'Последняя заметка'
+
+
+--Заметки--
+
+latest_notes =  'Последние заметки'
+notes_showall = 'Показать все'
+
+notes_create = 'Создание заметки'
+notes_name = 'Название'
+notes_text = 'Текст'
+notes_audio = 'Записать аудио'
+notes_cancel = 'Отменить'
+notes_add = 'Создать'
+notes_delete = 'Удалить'
+notes_listen = 'Прослушать'
+notes_edit = 'Редактировать'
+notes_save = 'ОК'
+notes_edit_title = 'Редактировать заметку'
+notes_changename = 'Изменить название'
+notes_changetext = 'Изменить текст'
+notes_rewrite_audio = 'Переписать аудио'
+
+
+
+--Календарь--
+
+closest_events_calendar = 'Ближайшие события календаря'
+events_onday = 'На день'
+events_onweek = 'На неделю'
+events_total = 'Всего событий'
+events_today = 'Событий за сегодня'
+events_yesterday = 'Событий за вчера'
+events_week = 'Событий за неделю'
+events_month = 'Событий за месяц'
+events_year = 'Событий за год'
+
+
+--Напоминания--
+
+latest_reminders = 'Последние напоминания'
+closed_reminders = 'Завершенные напоминания'
+reminders_showall = 'Всего напоминаний'
+total_reminders = 'Всего напоминаний'
+active_reminders = 'Активных напоминаний'
+
+
+
+--Личный кабинет --
+
+profile_nickname = 'Ник'
+profile_name = 'Имя'
+profile_surname = 'Фамилия'
+profile_email = 'Эл.почта'
+profile_edit = 'Редактировать профиль'
+profile_oldpass = 'Старый пароль'
+profile_newpass = 'Новый пароль'
+profile_newpassagain = 'Новый пароль еще раз'
+profile_change_avatar = 'Изменить аватар'
+profile_cancel = 'Отменить'
+profile_save = 'Сохранить'
+profile_change_password = 'Изменить пароль'
+profile_settings = 'Настройки'
+profile_tembrvoice = 'Выберите тембр голоса Морриса: '
+profile_lowvoice = 'Низкий'
+profile_mediumvoice = 'Средний'
+profile_highvoice = 'Высокий'
+profile_language = 'Выберите язык:'
+profile_russian = 'Русский'
+profile_english = 'Английский'
+profile_germany = 'Немецкий'
+
+
+
+
