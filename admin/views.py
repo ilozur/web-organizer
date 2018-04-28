037444cfe838c9bb7bfd6a0f841900d333a581a0
Andey Ryabov <Ryabov.a80@mail.ru>
2018-04-28 22:07:25 +0300

    add peredacha to template list of user

--- admin/views.py
+++ admin/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+from admin.models import User
+
 
 def index(request):
     context = {}
+    list_user = []
+    users = User.get_users()
+    for i in range(users):
+        list_user.append(i.userName,i.name, i.surname, i.added_time, i.is_voice, i.last_edit_time)
+    context['user_data'] = list_user
     return render(request, "admin/index.html", context)
+
+
+
+s
