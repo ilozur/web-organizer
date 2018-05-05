80c9683a916d78f03b0a4617392e4597d0ef8788
Andey Ryabov <Ryabov.a80@mail.ru>
2018-05-05 21:43:06 +0300

    clear models for usless

--- admin/models.py
+++ admin/models.py
@@ -1,14 +1,2 @@
 from django.db import models
 
-class User(models.Model):
-    userName = models.CharField(max_length=128)
-    name = models.CharField(max_length=128)
-    surename = models.CharField(max_length=128)
-    added_time = models.DateTimeField(auto_now_add=True)
-    is_voice = models.BooleanField(default=False)
-    last_edit_time = models.DateTimeField(default=None, null=True)
-
-
-def get_users():
-    users = User.objects.all()
-
