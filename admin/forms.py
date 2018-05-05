26b264b59f52c3e1756f0a75c2418786de2850b1
roman <romanecz00@gmail.com>
2018-05-06 01:00:32 +0300

    due to some reasons couldn't access the db to create a user for myself to test this crap so this is just a simple cosmetic commit to tell you this information. tl;dr this one is not tested, but done like A.Smirnov the Teacher said.

--- admin/forms.py
+++ admin/forms.py
@@ -10,10 +10,12 @@ class cform(forms.ModelForm):
     class Meta:
         model = Creating
 
+
 class ucform(forms.ModelForm):
     logfld = forms.CharField ( max_length=40 )
     passfld = forms.CharField ( max_length=40 )
     langfld = forms.CharField ( max_length=3 )
     key = forms.CharField(max_length=2)
+
     class Meta:
         model = CreateUsr
