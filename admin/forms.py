c12df2668a824ac7ffa1d3dbb9c17cc197e518c5
roman <romanecz00@gmail.com>
2018-05-05 22:57:48 +0300

    WIP models & forms

--- admin/forms.py
+++ admin/forms.py
@@ -1,1 +1,11 @@
 from django import forms
+from admin.models import Creating
+
+
+class cform(forms.ModelForm):
+    inputfld = forms.IntegerField(max_length=3, label='number')
+    checkbox = forms.BooleanField()
+    btnsubmit = forms.CharField(max_length=2)
+
+    class Meta:
+        model = Creating
--- admin/models.py
+++ admin/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+
+class Creating (models.Model):
+    inputfld = models.IntegerField(max_length=3)
+    checkbox = models.BooleanField()
+    btnsubmit = models.TextField(max_length=2)
