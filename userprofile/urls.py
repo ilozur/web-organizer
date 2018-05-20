9282043cb33f39233458f85600bcde53abc2e908
Bryantsev Stepan <steperb@gmail.com>
2018-05-20 13:25:18 +0300

    Added changing language

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -5,7 +5,8 @@ from userprofile.views import *
 urlpatterns = [
     path('', index, name='userprofile.index'),
     path('change_user_data', change_user_data_ajax, name='userprofile.change_user_data'),
-    path('change_password', change_password_ajax, name='userprofile.change_password'),
+    path('change_user_data', change_user_data_ajax, name='userprofile.change_user_data'),
+    path('change_language', change_language_ajax, name='userprofile.change_language'),
     path('get_user_data', get_user_data_ajax, name='userprofile.get_user_data'),
     path('recover_password_key', create_recover_password_key_ajax, name='userprofile.recover_password_key'),
     path('recover_password/<str:key>', recover_password_view, name='userprofile.recover_password'),
